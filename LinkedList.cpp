0307208fda9323ccfd58058bc645f5f0e6329629
Ranit Jana <ranitjana100@gmail.com>
2024-01-27 18:57:15 +0530

    add basic methods in linked list

---
+++ LinkedList.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SinglyLinkedList
+{
+    struct Node
+    {
+        int data;
+        Node *next;
+        Node(int value)
+        {
+            this->data = value;
+            this->next = NULL;
+        }
+    } *head;
+
+public:
+    SinglyLinkedList()
+    {
+        head = NULL;
+    }
+    void push_back(int value)
+    {
+        Node *newNode = new Node(value);
+        if (!head)
+        {
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    void push_front(int value)
+    {
+        Node *newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+    void insertAtPos(int value, int index)
+    {
+        if (index <= 0 || index > size() + 1)
+        {
+            cout << "Invalid position." << endl;
+            return;
+        }
+        if (index == 1)
+            push_front(value);
+        else
+        {
+            index--;
+            Node *temp = head;
+            while (--index)
+            {
+                temp = temp->next;
+            }
+            Node *newNode = new Node(value);
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void insertAfter(int value, int target)
+    {
+        Node *temp = head;
+        while (temp && temp->data != target)
+        {
+            temp = temp->next;
+        }
+        if (temp == NULL)
+        {
+            cout << target << " is not found.\n";
+            return;
+        }
+        Node *newNode = new Node(value);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+    void insertBefore(int value, int target)
+    {
+        Node *temp = head;
+        if (head == NULL)
+        {
+            cout << "Empty List.\n";
+            return;
+        }
+        Node *newNode = new Node(value);
+        if (head->data == target)
+        {
+            newNode->next = head;
+            head = newNode;
+            return;
+        }
+        while (temp->next && temp->next->data != target)
+            temp = temp->next;
+        if (temp->next == NULL)
+        {
+            cout << target << " is not present.\n";
+            delete newNode;
+            return;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+    void pop_back()
+    {
+        if (!head)
+        {
+            cout << "List is empty.\n";
+            return;
+        }
+        Node *temp = head;
+        while (temp->next && temp->next->next)
+        {
+            temp = temp->next;
+        }
+        if (temp->next == NULL)
+        { // single element
+            head = NULL;
+            delete temp;
+        }
+        else
+        {
+            Node *del = temp->next;
+            temp->next = NULL;
+            delete del;
+        }
+    }
+
+    void pop_front()
+    {
+        if (!head)
+        {
+            cout << "Enpty Linked Lis.\n";
+            return;
+        }
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
+    void display()
+    {
+        if (!head)
+        {
+            cout << "List is empty.\n";
+            return;
+        }
+        Node *temp = head;
+        while (temp->next)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << temp->data << endl;
+    }
+    int size()
+    {
+        Node *temp = head;
+        int count = 0;
+        while (temp)
+        {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    SinglyLinkedList li;
+    li.insertAtPos(50, 1);
+    li.insertAtPos(12, 1);
+    li.insertAtPos(50, 3);
+    li.insertAtPos(0, 2);
+    li.display();
+    return 0;
+}
